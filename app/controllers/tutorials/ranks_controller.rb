a53240e4fca8403f3effb3013e914100702daceb
TaigaMikami <mikamimikami60@gmail.com>
2018-09-06 03:11:26 +0900

    :+1: ランキングソート追加 PV数のソートといいね数のソート

--- app/controllers/tutorials/ranks_controller.rb
+++ app/controllers/tutorials/ranks_controller.rb
@@ -1,9 +1,9 @@
 class Tutorials::RanksController < ApplicationController
   def pv_rank
-
+    @tutorial = Tutorial.seven_day_post.sort_by { |t| t.impressionist_count }.reverse
   end
 
   def iine_rank
-
+    @tutorial = Tutorial.seven_day_post.sort_by { |t| t.users.count }.reverse
   end
 end
