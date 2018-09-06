1214c73b58e1a4b16121a273f5fbd0d82aa7af83
TaigaMikami <mikamimikami60@gmail.com>
2018-09-06 21:55:40 +0900

    :+1: ランキングの期間の絞り込み位置を修正

--- app/assets/javascripts/ranks/application.js
+++ app/assets/javascripts/ranks/application.js
@@ -1,3 +1,11 @@
 //= require lib/tag-it
 //= require_tree .
 
+$(document).on('turbolinks:load', function() {
+    var url = location.href
+    if (url.indexOf('all') != -1) {
+        $('#all').addClass('btn-active')
+    } else if (url.indexOf('/iine') != -1) {
+        $('#week').addClass('btn-active')
+    }
+})
