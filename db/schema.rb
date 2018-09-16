039098dc898ef7329c84e95e33c2e9567be370bc
TaigaMikami <mikamimikami60@gmail.com>
2018-09-16 19:33:38 +0900

    :sparkles: ユーザー一言追加 自己紹介の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_05_171051) do
+ActiveRecord::Schema.define(version: 2018_09_16_100547) do
 
   create_table "impressions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "impressionable_type"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 2018_09_05_171051) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
