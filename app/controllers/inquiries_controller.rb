db78f94a12b786e77bde1869b2a4cdb4ada16d89
TaigaMikami <mikamimikami60@gmail.com>
2018-10-22 23:37:02 +0900

    :sparkles: お問い合わせフォームのビュー作成

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,10 +1,24 @@
 class InquiriesController < ApplicationController
   def new
+    @inquiry = Inquiry.new
   end
 
   def confirm
+    @inquiry = Inquiry.new(inquiry_params)
   end
 
   def complete
+    @inquiry = Inquiry.new(inquiry_params)
+    if @inquiry.save
+
+    else
+
+    end
+  end
+
+  private
+
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :email, :message)
   end
 end
